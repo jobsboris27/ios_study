41fba030dfe8d545fc3898d8ec7b5dd22b17f92e
Boris <jobsboris27@gmail.com>
2020-12-01 16:54:24 +0300

    feat: make task 1 of hm 1

---
+++ SwiftBookCourse/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SwiftBookCourse
+//
+//  Created by Boris on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
